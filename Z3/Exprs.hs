df3586fac828070f72f0be56339b2de86deb4f1b
Iago Abal <5313615+IagoAbal@users.noreply.github.com>
2012-07-25 00:33:13 +0100

    Remove trailing spaces

--- Z3/Exprs.hs
+++ Z3/Exprs.hs
@@ -10,7 +10,7 @@
 -- Copyright : (c) Iago Abal, 2012
 --             (c) David Castro, 2012
 -- License   : BSD3
--- Maintainer: Iago Abal <iago.abal@gmail.com>, 
+-- Maintainer: Iago Abal <iago.abal@gmail.com>,
 --             David Castro <david.castro.dcp@gmail.com>
 -- Stability : experimental
 
--- Z3/Types/TY.hs
+++ Z3/Types/TY.hs
@@ -6,7 +6,7 @@
 -- Copyright : (c) Iago Abal, 2012
 --             (c) David Castro, 2012
 -- License   : BSD3
--- Maintainer: Iago Abal <iago.abal@gmail.com>, 
+-- Maintainer: Iago Abal <iago.abal@gmail.com>,
 --             David Castro <david.castro.dcp@gmail.com>
 
 module Z3.Types.TY where
