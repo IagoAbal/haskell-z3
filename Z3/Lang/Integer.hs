e33c76a345a7745ea38cd8cfc4e8533a1d5af9ce
David Castro <david.castro.dcp@gmail.com>
2012-10-03 02:29:30 +0100

    Added IsTy instances of Integer and Rational.

---
+++ Z3/Lang/Integer.hs
@@ -0,0 +1,54 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+-- |
+-- Module    : Z3.Lang.Integer
+-- Copyright : (c) Iago Abal, 2012
+--             (c) David Castro, 2012
+-- License   : BSD3
+-- Maintainer: Iago Abal <iago.abal@gmail.com>,
+--             David Castro <david.castro.dcp@gmail.com>
+-- Stability : experimental
+
+module Z3.Lang.Integer () where
+
+import Z3.Base ( AST )
+import Z3.Lang.Bool ()
+import Z3.Lang.Monad
+
+type instance TypeZ3 Integer = Integer
+
+instance IsTy Integer where
+  compile = compileInteger
+
+instance IsScalar Integer where
+  fromZ3Type = id
+  toZ3Type = id
+
+instance IsNum Integer where
+instance IsInt Integer where
+
+compileInteger :: Expr Integer -> Z3 (AST Integer)
+compileInteger (Lit a)
+  = mkLiteral (toZ3Type a)
+compileInteger (Const u)
+  = getConst u
+compileInteger (Neg e)
+  = mkUnaryMinus =<< compileInteger e
+compileInteger (CRingArith op es)
+  = mkCRingArith op =<< mapM compileInteger es
+compileInteger (IntArith op e1 e2)
+  = do e1' <- compileInteger e1
+       e2' <- compileInteger e2
+       mkIntArith op e1' e2'
+compileInteger (Ite eb e1 e2)
+  = do eb' <- compile eb
+       e1' <- compileInteger e1
+       e2' <- compileInteger e2
+       mkIte eb' e1' e2'
+compileInteger _
+    = error "Z3.Lang.Integer.compileInteger: Panic!\
+        \ Impossible constructor in pattern matching!"
---
+++ Z3/Lang/Rational.hs
@@ -0,0 +1,54 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+-- |
+-- Module    : Z3.Lang.Rational
+-- Copyright : (c) Iago Abal, 2012
+--             (c) David Castro, 2012
+-- License   : BSD3
+-- Maintainer: Iago Abal <iago.abal@gmail.com>,
+--             David Castro <david.castro.dcp@gmail.com>
+-- Stability : experimental
+
+module Z3.Lang.Rational () where
+
+import Z3.Base ( AST )
+import Z3.Lang.Bool ()
+import Z3.Lang.Monad
+
+type instance TypeZ3 Rational = Rational
+
+instance IsTy Rational where
+  compile = compileRational
+
+instance IsScalar Rational where
+  fromZ3Type = id
+  toZ3Type = id
+
+instance IsNum Rational where
+instance IsReal Rational where
+
+compileRational :: Expr Rational -> Z3 (AST Rational)
+compileRational (Lit a)
+  = mkLiteral (toZ3Type a)
+compileRational (Const u)
+  = getConst u
+compileRational (Neg e)
+  = mkUnaryMinus =<< compileRational e
+compileRational (CRingArith op es)
+  = mkCRingArith op =<< mapM compileRational es
+compileRational (RealArith op e1 e2)
+  = do e1' <- compileRational e1
+       e2' <- compileRational e2
+       mkRealArith op e1' e2'
+compileRational (Ite eb e1 e2)
+  = do eb' <- compile eb
+       e1' <- compileRational e1
+       e2' <- compileRational e2
+       mkIte eb' e1' e2'
+compileRational _
+    = error "Z3.Lang.Rational.compileRational: Panic!\
+        \ Impossible constructor in pattern matching!"
