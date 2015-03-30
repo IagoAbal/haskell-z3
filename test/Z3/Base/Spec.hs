202e428b11467193e7207bfd5f739619d3882ee2
Iago Abal <5313615+IagoAbal@users.noreply.github.com>
2015-03-30 12:22:23 +0200

    Set up a Hspec test suite

---
+++ test/Z3/Base/Spec.hs
@@ -0,0 +1,37 @@
+-- TODO: Create a context for all the tests, use push; ...; pop to rollback
+
+module Z3.Base.Spec
+  ( spec )
+  where
+
+import Test.Hspec
+import Test.QuickCheck ( property )
+import Test.QuickCheck.Monadic
+
+import qualified Z3.Base as Z3
+
+withContext :: ActionWith Z3.Context -> IO ()
+withContext k = do
+  ctx <- Z3.withConfig Z3.mkContext
+  k ctx
+
+spec :: Spec
+spec = around withContext $ do
+
+  context "Propositional Logic and Equality" $ do
+
+    specify "mkBool" $ \ctx -> property $ \b ->
+      monadicIO $ do
+        x::Maybe Bool <- run $ do
+          ast <- Z3.mkBool ctx b
+          Z3.getBool ctx ast
+        assert $ x == Just b
+
+  context "Numerals" $ do
+
+    specify "mkInt" $ \ctx -> property $ \(i :: Integer) ->
+      monadicIO $ do
+        x::Integer <- run $ do
+          ast <- Z3.mkInt ctx i;
+          Z3.getInt ctx ast;
+        assert $ x == i
