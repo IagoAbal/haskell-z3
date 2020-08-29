IagoAbal/haskell-z3

commit 6b61fbc031a246128ce22cc1efc2e179ea2d3670

Add optimization facilities

src/Z3/Base.hs
@@ -396,6 +396,32 @@ module Z3.Base (
   , fixedpointGetAnswer
   , fixedpointGetAssertions
 
+  -- * Optimization
+  , Optimize (..)
+  , mkOptimize
+  , optimizeAssert
+  , optimizeAssertAndTrack
+  , optimizeAssertSoft
+  , optimizeMaximize
+  , optimizeMinimize
+  , optimizePush
+  , optimizePop
+  , optimizeCheck
+  , optimizeGetReasonUnknown
+  , optimizeGetModel
+  , optimizeGetUnsatCore
+  , optimizeSetParams
+  , optimizeGetLower
+  , optimizeGetUpper
+  , optimizeGetUpperAsVector
+  , optimizeGetLowerAsVector
+  , optimizeToString
+  , optimizeFromString
+  , optimizeFromFile
+  , optimizeGetHelp
+  , optimizeGetAssertions
+  , optimizeGetObjectives
+
   -- * Solvers
   , Logic(..)
   , mkSolver
@@ -2584,6 +2610,91 @@ fixedpointGetAnswer = liftFun1 z3_fixedpoint_get_answer
 
 fixedpointGetAssertions :: Context -> Fixedpoint -> IO [AST]
 fixedpointGetAssertions = liftFun1 z3_fixedpoint_get_assertions
+
+---------------------------------------------------------------------
+-- Optimization facilities
+
+newtype Optimize = Optimize { unOptimize :: ForeignPtr Z3_optimize }
+    deriving Eq
+
+instance Marshal Optimize (Ptr Z3_optimize) where
+  c2h = mkC2hRefCount Optimize z3_optimize_inc_ref z3_optimize_dec_ref
+  h2c fp = withForeignPtr (unOptimize fp)
+
+mkOptimize :: Context -> IO Optimize
+mkOptimize = liftFun0 z3_mk_optimize
+
+optimizeAssert :: Context -> Optimize -> AST -> IO ()
+optimizeAssert = liftFun2 z3_optimize_assert
+
+optimizeAssertAndTrack :: Context -> Optimize -> AST -> AST -> IO ()
+optimizeAssertAndTrack = liftFun3 z3_optimize_assert_and_track
+
+optimizeAssertSoft :: Context -> Optimize -> AST -> String -> Symbol -> IO Int
+optimizeAssertSoft ctx opt ast str sym =
+  marshal z3_optimize_assert_soft ctx $ \f ->
+    h2c opt $ \optPtr ->
+    h2c ast $ \astPtr ->
+    withCString str $ \strPtr ->
+    h2c sym $ \symPtr ->
+      f optPtr astPtr strPtr symPtr
+
+optimizeMaximize :: Context -> Optimize -> AST -> IO Int
+optimizeMaximize = liftFun2 z3_optimize_maximize
+
+optimizeMinimize :: Context -> Optimize -> AST -> IO Int
+optimizeMinimize = liftFun2 z3_optimize_minimize
+
+optimizePush :: Context -> Optimize -> IO ()
+optimizePush = liftFun1 z3_optimize_push
+
+optimizePop :: Context -> Optimize -> IO ()
+optimizePop = liftFun1 z3_optimize_pop
+
+optimizeCheck :: Context -> Optimize -> IO Result
+optimizeCheck = liftFun1 z3_optimize_check
+
+optimizeGetReasonUnknown :: Context -> Optimize -> IO String
+optimizeGetReasonUnknown = liftFun1 z3_optimize_get_reason_unknown
+
+optimizeGetModel :: Context -> Optimize -> IO Model
+optimizeGetModel = liftFun1 z3_optimize_get_model
+
+optimizeGetUnsatCore :: Context -> Optimize -> IO [AST]
+optimizeGetUnsatCore = liftFun1 z3_optimize_get_unsat_core
+
+optimizeSetParams :: Context -> Optimize -> Params -> IO ()
+optimizeSetParams = liftFun2 z3_optimize_set_params
+
+optimizeGetLower :: Context -> Optimize -> Int -> IO AST
+optimizeGetLower = liftFun2 z3_optimize_get_lower
+
+optimizeGetUpper :: Context -> Optimize -> Int -> IO AST
+optimizeGetUpper = liftFun2 z3_optimize_get_upper
+
+optimizeGetUpperAsVector :: Context -> Optimize -> Int -> IO [AST]
+optimizeGetUpperAsVector = liftFun2 z3_optimize_get_upper_as_vector
+
+optimizeGetLowerAsVector :: Context -> Optimize -> Int -> IO [AST]
+optimizeGetLowerAsVector = liftFun2 z3_optimize_get_lower_as_vector
+
+optimizeToString :: Context -> Optimize -> IO String
+optimizeToString = liftFun1 z3_optimize_to_string
+
+optimizeFromString :: Context -> Optimize -> String -> IO ()
+optimizeFromString = liftFun2 z3_optimize_from_string
+
+optimizeFromFile :: Context -> Optimize -> String -> IO ()
+optimizeFromFile = liftFun2 z3_optimize_from_file
+ 
+optimizeGetHelp :: Context -> Optimize -> IO String
+optimizeGetHelp = liftFun1 z3_optimize_get_help
+
+optimizeGetAssertions :: Context -> Optimize -> IO [AST]
+optimizeGetAssertions = liftFun1 z3_optimize_get_assertions
+
+optimizeGetObjectives :: Context -> Optimize -> IO [AST]
+optimizeGetObjectives = liftFun1 z3_optimize_get_objectives
 
 -- AST vectors ?
 

src/Z3/Base/C.hsc
@@ -68,6 +68,8 @@ data Z3_func_entry
 
 data Z3_fixedpoint
 
+data Z3_optimize
+
 data Z3_solver
 
 data Z3_params
@@ -1508,3 +1510,137 @@ foreign import ccall unsafe "Z3_fixedpoint_query_relations"
                                   -> CUInt
                                   -> Ptr (Ptr Z3_func_decl)
                                   -> IO Z3_lbool
+
+---------------------------------------------------------------------
+-- * Optimization facilities
+
+foreign import ccall unsafe "Z3_mk_optimize"
+    z3_mk_optimize :: Ptr Z3_context -> IO (Ptr Z3_optimize)
+
+foreign import ccall unsafe "Z3_optimize_inc_ref"
+    z3_optimize_inc_ref :: Ptr Z3_context -> Ptr Z3_optimize -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_dec_ref"
+    z3_optimize_dec_ref :: Ptr Z3_context -> Ptr Z3_optimize -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_assert"
+    z3_optimize_assert :: Ptr Z3_context -> Ptr Z3_optimize -> Ptr Z3_ast -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_assert_and_track"
+    z3_optimize_assert_and_track :: Ptr Z3_context
+                                 -> Ptr Z3_optimize
+                                 -> Ptr Z3_ast
+                                 -> Ptr Z3_ast
+                                 -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_assert_soft"
+    z3_optimize_assert_soft :: Ptr Z3_context
+                            -> Ptr Z3_optimize
+                            -> Ptr Z3_ast
+                            -> Z3_string
+                            -> Ptr Z3_symbol
+                            -> IO CUInt
+
+foreign import ccall unsafe "Z3_optimize_maximize"
+    z3_optimize_maximize :: Ptr Z3_context
+                         -> Ptr Z3_optimize
+                         -> Ptr Z3_ast
+                         -> IO CUInt
+
+foreign import ccall unsafe "Z3_optimize_minimize"
+    z3_optimize_minimize :: Ptr Z3_context
+                         -> Ptr Z3_optimize
+                         -> Ptr Z3_ast
+                         -> IO CUInt
+
+foreign import ccall unsafe "Z3_optimize_push"
+    z3_optimize_push :: Ptr Z3_context
+                     -> Ptr Z3_optimize
+                     -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_pop"
+    z3_optimize_pop :: Ptr Z3_context
+                    -> Ptr Z3_optimize
+                    -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_check"
+    z3_optimize_check :: Ptr Z3_context
+                      -> Ptr Z3_optimize
+                      -> IO Z3_lbool
+
+foreign import ccall unsafe "Z3_optimize_get_reason_unknown"
+    z3_optimize_get_reason_unknown :: Ptr Z3_context
+                                   -> Ptr Z3_optimize
+                                   -> IO Z3_string
+
+foreign import ccall unsafe "Z3_optimize_get_model"
+    z3_optimize_get_model :: Ptr Z3_context
+                          -> Ptr Z3_optimize
+                          -> IO (Ptr Z3_model)
+
+foreign import ccall unsafe "Z3_optimize_get_unsat_core"
+    z3_optimize_get_unsat_core :: Ptr Z3_context
+                               -> Ptr Z3_optimize
+                               -> IO (Ptr Z3_ast_vector)
+
+foreign import ccall unsafe "Z3_optimize_set_params"
+    z3_optimize_set_params :: Ptr Z3_context
+                           -> Ptr Z3_optimize
+                           -> Ptr Z3_params
+                           -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_get_lower"
+    z3_optimize_get_lower :: Ptr Z3_context
+                          -> Ptr Z3_optimize
+                          -> CUInt
+                          -> IO (Ptr Z3_ast)
+
+foreign import ccall unsafe "Z3_optimize_get_upper"
+    z3_optimize_get_upper :: Ptr Z3_context
+                          -> Ptr Z3_optimize
+                          -> CUInt
+                          -> IO (Ptr Z3_ast)
+
+foreign import ccall unsafe "Z3_optimize_get_lower_as_vector"
+    z3_optimize_get_lower_as_vector :: Ptr Z3_context
+                                    -> Ptr Z3_optimize
+                                    -> CUInt
+                                    -> IO (Ptr Z3_ast_vector)
+
+foreign import ccall unsafe "Z3_optimize_get_upper_as_vector"
+    z3_optimize_get_upper_as_vector :: Ptr Z3_context
+                                    -> Ptr Z3_optimize
+                                    -> CUInt
+                                    -> IO (Ptr Z3_ast_vector)
+
+foreign import ccall unsafe "Z3_optimize_to_string"
+    z3_optimize_to_string :: Ptr Z3_context
+                          -> Ptr Z3_optimize
+                          -> IO Z3_string
+
+foreign import ccall unsafe "Z3_optimize_from_string"
+    z3_optimize_from_string :: Ptr Z3_context
+                            -> Ptr Z3_optimize
+                            -> Z3_string
+                            -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_from_file"
+    z3_optimize_from_file :: Ptr Z3_context
+                          -> Ptr Z3_optimize
+                          -> Z3_string
+                          -> IO ()
+
+foreign import ccall unsafe "Z3_optimize_get_help"
+    z3_optimize_get_help :: Ptr Z3_context
+                         -> Ptr Z3_optimize
+                         -> IO Z3_string
+
+foreign import ccall unsafe "Z3_optimize_get_assertions"
+    z3_optimize_get_assertions :: Ptr Z3_context
+                               -> Ptr Z3_optimize
+                               -> IO (Ptr Z3_ast_vector)
+
+foreign import ccall unsafe "Z3_optimize_get_objectives"
+    z3_optimize_get_objectives :: Ptr Z3_context
+                               -> Ptr Z3_optimize
+                               -> IO (Ptr Z3_ast_vector)

src/Z3/Monad.hs
@@ -352,6 +352,32 @@ module Z3.Monad
   , fixedpointGetAnswer
   , fixedpointGetAssertions
 
+  -- * Optimization
+  , Optimize
+  , optimizeAssert
+  , optimizeAssertAndTrack
+  , optimizeAssertSoft
+  , optimizeMaximize
+  , optimizeMinimize
+  , optimizePush
+  , optimizePop
+  , optimizeCheck
+  , optimizeGetReasonUnknown
+  , optimizeGetModel
+  , optimizeGetUnsatCore
+  , optimizeSetParams
+  , optimizeGetLower
+  , optimizeGetUpper
+  , optimizeGetLower
+  , optimizeGetUpperAsVector
+  , optimizeGetLowerAsVector
+  , optimizeToString
+  , optimizeFromString
+  , optimizeFromFile
+  , optimizeGetHelp
+  , optimizeGetAssertions
+  , optimizeGetObjectives
+
   -- * Solvers
   , solverGetHelp
   , solverSetParams
@@ -404,6 +430,7 @@ import Z3.Base
   , Params
   , Solver
   , Fixedpoint
+  , Optimize
   , SortKind(..)
   , ASTKind(..)
   , Tactic
@@ -510,6 +537,27 @@ liftFixedpoint2 f a b = do
   slv <- getFixedpoint
   liftIO $ f ctx slv a b
 
+liftOptimize0 :: MonadOptimize z3 =>
+       (Base.Context -> Base.Optimize -> IO b)
+    -> z3 b
+liftOptimize0 f_s =
+  do ctx <- getContext
+     liftIO . f_s ctx =<< getOptimize
+
+liftOptimize1 :: MonadOptimize z3 =>
+       (Base.Context -> Base.Optimize -> a -> IO b)
+    -> a -> z3 b
+liftOptimize1 f_s a =
+  do ctx <- getContext
+     liftIO . (\s -> f_s ctx s a) =<< getOptimize
+
+liftOptimize2 :: MonadOptimize z3 => (Base.Context -> Base.Optimize -> a -> b -> IO c)
+                             -> a -> b -> z3 c
+liftOptimize2 f a b = do
+  ctx <- getContext
+  slv <- getOptimize
+  liftIO $ f ctx slv a b
+
 -------------------------------------------------
 -- A simple Z3 monad.
 
@@ -522,6 +570,7 @@ data Z3Env
       envSolver     :: Base.Solver
     , envContext    :: Base.Context
     , envFixedpoint :: Base.Fixedpoint
+    , envOptimize   :: Base.Optimize
     }
 
 instance MonadZ3 Z3 where
@@ -530,6 +579,9 @@ instance MonadZ3 Z3 where
 
 instance MonadFixedpoint Z3 where
   getFixedpoint = Z3 $ asks envFixedpoint
+
+instance MonadOptimize Z3 where
+  getOptimize = Z3 $ asks envOptimize
 
 -- | Eval a Z3 script.
 evalZ3With :: Maybe Logic -> Opts -> Z3 a -> IO a
@@ -549,7 +601,8 @@ newEnvWith mkContext mbLogic opts =
     ctx <- mkContext cfg
     solver <- maybe (Base.mkSolver ctx) (Base.mkSolverForLogic ctx) mbLogic
     fixedpoint <- Base.mkFixedpoint ctx
-    return $ Z3Env solver ctx fixedpoint
+    optimize <- Base.mkOptimize ctx
+    return $ Z3Env solver ctx fixedpoint optimize
 
 -- | Create a new Z3 environment.
 newEnv :: Maybe Logic -> Opts -> IO Z3Env
@@ -2033,6 +2086,78 @@ fixedpointGetAnswer = liftFixedpoint0 Base.fixedpointGetAnswer
 
 fixedpointGetAssertions :: MonadFixedpoint z3 => z3 [AST]
 fixedpointGetAssertions = liftFixedpoint0 Base.fixedpointGetAssertions
+
+---------------------------------------------------------------------
+-- Optimization
+
+class MonadZ3 m => MonadOptimize m where
+  getOptimize :: m Base.Optimize
+
+optimizeAssert :: MonadOptimize z3 => AST -> z3 ()
+optimizeAssert = liftOptimize1 Base.optimizeAssert
+
+optimizeAssertAndTrack :: MonadOptimize z3 => AST -> AST -> z3 ()
+optimizeAssertAndTrack = liftOptimize2 Base.optimizeAssertAndTrack
+
+optimizeAssertSoft :: MonadOptimize z3 => AST -> String -> Symbol -> z3 ()
+optimizeAssertSoft = undefined
+
+optimizeMaximize :: MonadOptimize z3 => AST -> z3 Int
+optimizeMaximize = liftOptimize1 Base.optimizeMaximize 
+
+optimizeMinimize :: MonadOptimize z3 => AST -> z3 Int
+optimizeMinimize = liftOptimize1 Base.optimizeMinimize 
+
+optimizePush :: MonadOptimize z3 => z3 ()
+optimizePush = liftOptimize0 Base.optimizePush
+
+optimizePop :: MonadOptimize z3 => z3 ()
+optimizePop = liftOptimize0 Base.optimizePop 
+
+optimizeCheck :: MonadOptimize z3 => z3 Result
+optimizeCheck = liftOptimize0 Base.optimizeCheck
+
+optimizeGetReasonUnknown :: MonadOptimize z3 => z3 String
+optimizeGetReasonUnknown = liftOptimize0 Base.optimizeGetReasonUnknown
+
+optimizeGetModel :: MonadOptimize z3 => z3 Model
+optimizeGetModel = liftOptimize0 Base.optimizeGetModel
+
+optimizeGetUnsatCore :: MonadOptimize z3 => z3 [AST]
+optimizeGetUnsatCore = liftOptimize0 Base.optimizeGetUnsatCore
+
+optimizeSetParams :: MonadOptimize z3 => Params -> z3 ()
+optimizeSetParams = liftOptimize1 Base.optimizeSetParams
+
+optimizeGetLower :: MonadOptimize z3 => Int -> z3 AST
+optimizeGetLower = liftOptimize1 Base.optimizeGetLower
+
+optimizeGetUpper :: MonadOptimize z3 => Int -> z3 AST
+optimizeGetUpper = liftOptimize1 Base.optimizeGetLower
+
+optimizeGetUpperAsVector :: MonadOptimize z3 => Int -> z3 [AST]
+optimizeGetUpperAsVector = liftOptimize1 Base.optimizeGetUpperAsVector
+
+optimizeGetLowerAsVector :: MonadOptimize z3 => Int -> z3 [AST]
+optimizeGetLowerAsVector = liftOptimize1 Base.optimizeGetLowerAsVector
+
+optimizeToString :: MonadOptimize z3 => z3 String
+optimizeToString = liftOptimize0 Base.optimizeToString
+
+optimizeFromString :: MonadOptimize z3 => String -> z3 ()
+optimizeFromString = liftOptimize1 Base.optimizeFromString
+
+optimizeFromFile :: MonadOptimize z3 => String -> z3 ()
+optimizeFromFile = liftOptimize1 Base.optimizeFromFile
+ 
+optimizeGetHelp :: MonadOptimize z3 => z3 String
+optimizeGetHelp = liftOptimize0 Base.optimizeGetHelp
+
+optimizeGetAssertions :: MonadOptimize z3 => z3 [AST]
+optimizeGetAssertions = liftOptimize0 Base.optimizeGetAssertions
+
+optimizeGetObjectives :: MonadOptimize z3 => z3 [AST]
+optimizeGetObjectives = liftOptimize0 Base.optimizeGetObjectives
 
 ---------------------------------------------------------------------
 -- * Solvers

commit e7564b132c2b29880acbdcec5560b0ae73fd93d0

Fix optimizeCheck and export Fixedpoint/Optimize monads (#41)
Fix optimizeCheck and export Fixedpoint and Optimize monads

Fixes: 6b61fbc031a ("Add optimization facilities")

src/Z3/Base.hs
@@ -2890,8 +2890,11 @@ optimizePush = liftFun1 z3_optimize_push
 optimizePop :: Context -> Optimize -> IO ()
 optimizePop = liftFun1 z3_optimize_pop
 
-optimizeCheck :: Context -> Optimize -> IO Result
-optimizeCheck = liftFun1 z3_optimize_check
+optimizeCheck :: Context -> Optimize -> [AST] -> IO Result
+optimizeCheck ctx opt ss = marshal z3_optimize_check ctx $ \f ->
+  h2c opt $ \optPtr ->
+  marshalArrayLen ss $ \ssNum ssPtr ->
+    f optPtr ssNum ssPtr
 
 optimizeGetReasonUnknown :: Context -> Optimize -> IO String
 optimizeGetReasonUnknown = liftFun1 z3_optimize_get_reason_unknown

src/Z3/Base/C.hsc
@@ -1674,6 +1674,8 @@ foreign import ccall unsafe "Z3_optimize_pop"
 foreign import ccall unsafe "Z3_optimize_check"
     z3_optimize_check :: Ptr Z3_context
                       -> Ptr Z3_optimize
+                      -> CUInt
+                      -> Ptr (Ptr Z3_ast)
                       -> IO Z3_lbool
 
 foreign import ccall unsafe "Z3_optimize_get_reason_unknown"

src/Z3/Monad.hs
@@ -381,6 +381,7 @@ module Z3.Monad
   , getVersion
 
   -- * Fixedpoint
+  , MonadFixedpoint(..)
   , Fixedpoint
   , fixedpointAddRule
   , fixedpointSetParams
@@ -390,6 +391,7 @@ module Z3.Monad
   , fixedpointGetAssertions
 
   -- * Optimization
+  , MonadOptimize(..)
   , Optimize
   , optimizeAssert
   , optimizeAssertAndTrack
@@ -2336,8 +2338,8 @@ optimizePush = liftOptimize0 Base.optimizePush
 optimizePop :: MonadOptimize z3 => z3 ()
 optimizePop = liftOptimize0 Base.optimizePop 
 
-optimizeCheck :: MonadOptimize z3 => z3 Result
-optimizeCheck = liftOptimize0 Base.optimizeCheck
+optimizeCheck :: MonadOptimize z3 => [AST] -> z3 Result
+optimizeCheck = liftOptimize1 Base.optimizeCheck
 
 optimizeGetReasonUnknown :: MonadOptimize z3 => z3 String
 optimizeGetReasonUnknown = liftOptimize0 Base.optimizeGetReasonUnknown